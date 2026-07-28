9fbce2501f082c607adb8211208ed990acedcbcd
nallamharshitha15 <nallamharshitha@gmail.com>
2026-07-28 13:25:42 +0530

    tokes-day1

---
+++ day 1.py
@@ -0,0 +1,15 @@
+print('hello world')
+#IDLE is color coding
+#hastag  is used to single line
+#multiple line ''',"""
+'''
+5 tokens
+'''
+
+name='sree'
+print("name:",name)
+mail_id='nallamharshitha@gmail.com'
+print("email_id:",mail_id)
+name='abc'
+print(name)
+
